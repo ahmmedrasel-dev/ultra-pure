cea83c3231906741382352ea2565429bb7309c4f
Ahmmed Rasel <rahmmed.info@gmail.com>
2023-03-14 22:14:18 +0600

    update website

--- src/pages/Home/Slider/Slide.js
+++ src/pages/Home/Slider/Slide.js
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import wsp from '../../../assets/images/wsp.jpeg'
 import ro from '../../../assets/images/ro.jpeg'
 import eog from '../../../assets/images/eog.jpeg'
+import construction from '../../../assets/images/under_maintaines.png'
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 
 // Import Swiper styles
@@ -13,7 +14,7 @@ import './slider.css'
 
 // import required modules
 import { Autoplay, Navigation, Pagination } from "swiper";
-import { Fade, LightSpeed, Slide as SlAni } from 'react-reveal';
+import { Slide as SlAni } from 'react-reveal';
 
 const Slide = () => {
   return (
@@ -79,6 +80,34 @@ const Slide = () => {
 
                 <SlAni right duration="2000">
                   <button className='btn border-0 bg-warning text-white hover:bg-primary lg:w-44 '>Lern More</button>
+                </SlAni>
+              </div>
+
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="slider-content" data-swiper-parallax="-300">
+            <img src={construction} className="w-full h-[600px]" alt="" />
+
+            <div className='absolute top-[20%] left-[50%] -translate-x-1/2'>
+              <div className="flex gap-8 flex-col items-center">
+                <SlAni left >
+                  <h2 className='text-2xl text-warning font-bold lg:text-5xl  text-center'>We are Working Our Website</h2>
+
+                  <p className='text-white text-center font-semibold'>
+                    We apologize for any inconvenience caused by the current state of our website. We are currently working on some updates and improvements to enhance your browsing experience.
+                    <br />
+                    Please bear with us as we strive to make our website more user-friendly and functional. We will do our best to complete the updates as soon as possible.
+                    <br />
+                    Thank you for your patience and understanding. Please feel free to contact us if you have any questions or concerns.
+                  </p>
+
+                </SlAni>
+
+                <SlAni right duration="2000">
+                  <button className='btn border-0 bg-warning text-white hover:bg-primary lg:w-56 '>Call: +8801860629988</button>
                 </SlAni>
               </div>
 
