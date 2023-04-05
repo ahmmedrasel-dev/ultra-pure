514bbfc2cd27ef4de658384863fdd3b912569f30
Ahmmed Rasel <rahmmed.info@gmail.com>
2023-04-06 02:37:25 +0600

    update feature section

--- src/pages/Home/Feature/Feature.js
+++ src/pages/Home/Feature/Feature.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { FaUsers } from "react-icons/fa";
-import { MdOutlineDiversity2 } from "react-icons/md";
-import { RiServiceFill } from "react-icons/ri";
+import { HiUserGroup } from "react-icons/hi2";
+import { FaHandHoldingWater } from "react-icons/fa";
+import { MdWorkspacePremium } from "react-icons/md";
 import SingleFeture from './SingleFeture';
 
 const Feature = () => {
   const features = [
     {
       "_id": 1,
-      "icon": <MdOutlineDiversity2 />,
-      "title": "Reliable Sales Service",
-      "summary": "simply dummy text of the printing and typesetting industry. Lorem Ipsum has"
+      "icon": <FaHandHoldingWater />,
+      "title": "Customized Solutions",
+      "summary": "Customized solutions for each client based on water quality issues, usage patterns, and regulatory requirements ensure maximum effectiveness and efficiency."
     },
     {
       "_id": 2,
-      "icon": <FaUsers />,
-      "title": "Meet Our Expertise",
-      "summary": "simply dummy text of the printing and typesetting industry. Lorem Ipsum has"
+      "icon": <HiUserGroup />,
+      "title": "Experienced Staff",
+      "summary": "Our experienced staff of engineers and technicians are experts in the latest water treatment technologies and methods, ensuring effective solutions for our clients."
     },
     {
       "_id": 3,
-      "icon": <RiServiceFill />,
-      "title": "Project Solution",
-      "summary": "simply dummy text of the printing and typesetting industry. Lorem Ipsum has"
+      "icon": <MdWorkspacePremium />,
+      "title": "Hight Quality Products",
+      "summary": "We offer high-quality water treatment products, including filtration systems, water softeners, and reverse osmosis systems, ensuring long-lasting solutions for our clients."
     }
   ]
   return (
--- src/pages/Home/Feature/SingleFeture.js
+++ src/pages/Home/Feature/SingleFeture.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BsArrowRightShort } from 'react-icons/bs'
 
 
 const SingleFeture = ({ feature }) => {
   const { icon, title, summary } = feature;
   return (
-    <div className='flex flex-col items-center bg-slate-50 rounded-lg p-10 shadow-md'>
-      <div className='text-6xl text-warning bg-pure p-4 rounded-full shadow-lg'>{icon}</div>
-      <h2 className='py-4 text-2xl font-semibold text-center'>{title}</h2>
-      <p className='text-center'>{summary}</p>
+    <div className='bg-slate-50 px-6 py-8 shadow-lg hover:bg-primary hover:text-white duration-500'>
+      <p className='text-8xl mb-2 text-warning'>{icon}</p>
+      <h2 className='text-xl font-semibold '>{title}</h2>
+      <p className='py-4 '>{summary}</p>
+      <Link className='text-warning flex items-center hover:text-white'>Discover Now <BsArrowRightShort className='text-3xl' /></Link>
     </div>
   );
 };
